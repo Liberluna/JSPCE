2d92f146c6671bd78c9a6bc05eba9f584f68d976
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-11-18 09:36:14 +0900

    add

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap()],
-  site: "https://jspce.ame-x.net",
-  // 暫定
+  site: "https://jspce.ame-x.net", // 暫定
 });
