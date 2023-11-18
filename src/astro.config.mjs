6f3f36e1f9557a64df3610aba4e920a97df5ea3a
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-11-18 12:59:38 +0900

    add header & switcher

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), react()],
   site: "https://jspce.ame-x.net", // 暫定
 });
