97d6a8d34bea0342a57a79aed5174d4871915f1b
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-11-18 16:52:09 +0900

    fix

--- src/tailwind.config.mjs
+++ src/tailwind.config.mjs
@@ -4,6 +4,40 @@ export default {
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		function({ addUtilities }) {
+			const newUtilities = {
+			  ".text-shadow": {
+				textShadow: "0px 2px 3px white"
+			  },
+			  ".text-shadow-xs": {
+				textShadow: "0px 1px 3px white"
+			  },
+			  ".text-shadow-sm": {
+				textShadow: "0px 2px 3px white"
+			  },
+			  ".text-shadow-md": {
+				textShadow: "0px 3px 3px white"
+			  },
+			  ".text-shadow-lg": {
+				textShadow: "0px 5px 3px white"
+			  },
+			  ".text-shadow-xl": {
+				textShadow: "0px 7px 3px white"
+			  },
+			  ".text-shadow-2xl": {
+				textShadow: "0px 10px 3px white"
+			  },
+			  ".text-shadow-3xl": {
+				textShadow: "0px 15px 3px white"
+			  },
+			  ".text-shadow-none": {
+				textShadow: "none"
+			  }
+			};
+	  
+			addUtilities(newUtilities);
+		  }
+	], // text-shadow-[power]
 	darkMode: "class",
 }
