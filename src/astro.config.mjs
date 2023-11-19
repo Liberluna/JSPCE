48e8b7db845f0fafb504781649843a8849444892
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-11-19 17:26:49 +0900

    change to vercel

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -2,18 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import node from "@astrojs/node";
-import cloudflare from "@astrojs/cloudflare";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), sitemap()],
   output: "server",
-  adapter: cloudflare({
-    routes: {
-      exclude: [
-        "./dist/**/*",
-      ]
-    }
-  }),
+  adapter: vercel()
 });
