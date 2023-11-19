fe3d38d14a49a960f3e57244f59cbd952320e984
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-11-19 16:39:20 +0900

    convert SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react(), tailwind(), sitemap()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
