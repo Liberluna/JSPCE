25692762aaa832caf37f478d7b61e7b93bc5fa5f
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-11-21 19:12:55 +0900

    wip

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -2,12 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), sitemap()],
+  integrations: [react(), tailwind(), sitemap(), vue()],
   output: "server",
   adapter: vercel()
 });
---
+++ src/src/components/Loader/Loading.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { FingerprintSpinner } from "epic-spinners";
+</script>
+
+<template>
+  <div
+    :class="
+      'fixed z-[1] w-full h-full flex items-center justify-center'
+    "
+  >
+    <fingerprint-spinner
+      :animation-duration="500"
+      :size="100"
+      color="#efefef"
+    />
+  </div>
+</template>
+
+<style scoped>
+</style>
